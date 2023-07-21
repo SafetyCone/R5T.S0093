cdb7916158eef34e520bca5cced6a422debbb56e
davidcoats <david@getsafetycone.com>
2023-07-21 16:46:09 -0700

    Initial commit.

---
+++ source/R5T.S0093/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0093
+{
+	/// <summary>
+	/// Organization directory path scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0093/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.S0093
+{
+    public static class Instances
+    {
+		
+    }
+}
